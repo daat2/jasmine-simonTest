8fdd463dac42601899a01ec1f38e30cc2f342c5d
daat2 <ibi.olu@gmail.com>
2020-06-25 10:48:27 +0000

    add strict button spec

--- jasmine-testing/spec/gameSpec.js
+++ jasmine-testing/spec/gameSpec.js
@@ -21,6 +21,18 @@ describe("click on start buttton"),function(){
         $(startbutton.click)()
         expect(clicksound).toBe(sound)
         
-    })
+    });
+};
+};
+
+/******strict button *** */
+describe("strict button test"),function(){
+describe ("strict button check mark"),function(){
+    beforeEach(function(){
+        power=true;
+        it("should have a check mark")
+        $(strictbutton.click)
+        expect(checkmark).toBe(checked)
+})
 }
 }
